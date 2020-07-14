87d1b3e87ec5c29a4de02f7ab19d56089f966b04
Andy Foster <andrewfosteris@gmail.com>
2020-07-15 01:35:34 +1200

    Updates

---
+++ precache-manifest.d2f9f26318cc61bed5b8a710a181dd89.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bda3baf78619bad199ee5d92cbaff0f",
+    "url": "/breaking-bad-react/index.html"
+  },
+  {
+    "revision": "4e36ec33e1c839f36ccd",
+    "url": "/breaking-bad-react/static/css/main.a91b1a59.chunk.css"
+  },
+  {
+    "revision": "faba816f549ea4212350",
+    "url": "/breaking-bad-react/static/js/2.78da1dff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-react/static/js/2.78da1dff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e36ec33e1c839f36ccd",
+    "url": "/breaking-bad-react/static/js/main.98051ffe.chunk.js"
+  },
+  {
+    "revision": "759a66aaabdb90aff1e8",
+    "url": "/breaking-bad-react/static/js/runtime-main.ea152501.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-react/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-react/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-react/static/media/spinner.235b9b3e.gif"
+  }
+]);
