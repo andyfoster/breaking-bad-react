5b525c909297e83a732e6a421b1d836d3b9db964
Andy Foster <andrewfosteris@gmail.com>
2020-07-14 21:47:01 +1200

    Working image grid pulling from public API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+
+      // console.log(result.data);
+      setItems(result.data);
+      setisLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <CharacterGrid items={items} isLoading={isLoading} />
     </div>
   );
-}
+};
 
 export default App;
